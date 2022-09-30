21f38bd2414652fa9621f664ff3c10c1f505efb1
Adrian Lee <al772@cornell.edu>
2022-09-30 18:02:05 -0400

    Final update for 2022

--- sizing2022.m
+++ sizing2022.m
@@ -6,13 +6,13 @@ clc
 m2ft = 3.281;
 m2in = 3.281*12;
 mtow = 4; %kg (to vary)
-to_dist = 40/m2ft; %meters
+to_dist = 50/m2ft; %meters
 g = 9.81;%m/s^2
 wcl = 14; %(to range between 10-14), a measure of how flyable the RC plane is
 kg2oz = 35.274;
 sqin2sqm = 1/1550;
 rho = 1.225; %Standard air density
-ar = 7; % to range from 6-10
+ar = 6; % to range from 6-10
 
 %% Airfoil Stuff
 clmax = [1.324; 1.442; 1.48; 1.49; 1.585; 1.757]; %ClarkV, NACA4412,SD7032, USA35b, SD7062, MH114
@@ -26,8 +26,6 @@ S=144*(mtow*kg2oz/wcl)^(2/3)*sqin2sqm; %from https://www.sefsd.org/general-inter
 b=sqrt(S*ar); %AR = b^2/S
 b_in = b*m2ft*12;
 c = b/ar; %For a rectangular wing
-
-w_by_airfoils = c*max_thickness*4;
 
 l_box = b/2; %m
 l_box_in = l_box*m2ft*12;
@@ -43,33 +41,28 @@ F_thrust = F_drag + mtow*groundaccel; %N, minimum required, likely an underrepre
 T_to_W = F_thrust./(mtow*g);
 
 %% Fuselage
-totalLength = 0.75*b; %estimated from last year lmao
 max_fuselage_width = 0.2*b; %by comp rules
 max_fuselage_width_in = 0.2*b*m2ft*12;
-fuselage_width = 7/m2in; %7 inch (to be confirmed by mech)
-fuselage_length = 1.6*mtow^0.23; %From Raymer pg 157, this is bad for us ;-;
-mfw_in = max_fuselage_width*m2ft*12;
+fuselage_width = 5/m2in; %5 inch (confirmed by mech)
+fuselage_height = 5/m2in; %^
+fuselage_length = 30/m2in; %Estimated from payloads required
 
 %% Tail Sizing
 Vv = 0.04; % Vertical tail volume ratio from Raymer Aircraft Design pg. 160
 Vh = 0.7; % ^, but for hortizontal stab
-tailarm = 1.1 ;%0.67*totalLength; %estimated from last year lmao
+tailarm = 1.0 ; %Played around with this to get a good number
 Sv = Vv*b*S/tailarm; %From Raymer pg. 159
 Sh = Vh*c*S/tailarm; %From Raymer pg. 159
-ARh = 3;
-ARv = 1.5;
+ARh = 2.5; %Varied from 2-3
+ARv = 1.5; % Given by Aero
 
-b_h = sqrt(Sh*ARh);
-c_h = b_h/ARh;
+b_h = sqrt(Sh*ARh); %Same as eqn for wingspan
+b_h_in = b_h*m2in;
+c_h = b_h/ARh; %Same as equation for wing chord
 
-lambdav = 0.7;
+lambdav = 0.7; %From aero, empirical data from last year
 
-nvtail = 1;
-if (nvtail == 1)
-    b_v = sqrt(Sv*ARv);
-else
-    b_v = sqrt(0.5*Sv*ARv);
-end
-cv_root = 2*b_v/(ARv*(1+lambdav));
-
-%.5087
+b_v = sqrt(Sv*ARv);%Same as eqn for wingspan
+b_v_in = b_v*m2in;
+cv_root = 2*b_v/(ARv*(1+lambdav)); %Same eqn as wingspan, but with m.a.c instead
+cv_tip = lambdav*cv_root; %lambda=ctip/croot
